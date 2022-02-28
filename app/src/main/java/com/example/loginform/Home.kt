05cc5b9d852adbd048e9d1f7e3197c9dc8e8b86a
Hritik602 <hritikrajkarn@gmail.com>
2022-02-28 14:18:53 +0545

    Notification Button Created

--- app/src/main/java/com/example/loginform/Home.kt
+++ app/src/main/java/com/example/loginform/Home.kt
@@ -2,6 +2,7 @@ package com.example.loginform
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import org.w3c.dom.Text
@@ -10,10 +11,16 @@ class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-//
-//        val text =findViewById<EditText>(R.id.editTextTextEmailAddress)
-//
-//        println(text.text)
+
+        // Showing button
+        val clickedButton=findViewById<Button>(R.id.button)
+
+
+
+        //Button Function
+        clickedButton.setOnClickListener{
+            println("Clicked")
+        }
 
 
     }
